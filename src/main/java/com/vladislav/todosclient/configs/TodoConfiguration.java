def313c1674b65f9a9b967438eb663d521f376b5
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-08-23 20:58:17 +0300

    refactor

--- src/main/java/com/vladislav/todosclient/configs/TodoConfiguration.java
+++ src/main/java/com/vladislav/todosclient/configs/TodoConfiguration.java
@@ -2,12 +2,12 @@ package com.vladislav.todosclient.configs;
 
 import com.proto.todo.ProjectServiceGrpc;
 import com.proto.todo.TaskServiceGrpc;
-import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.spring.annotation.VaadinSessionScope;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Metadata;
+import io.grpc.stub.AbstractStub;
 import io.grpc.stub.MetadataUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -30,7 +30,16 @@ public class TodoConfiguration {
     @Bean
     @Scope("prototype")
     public TaskServiceGrpc.TaskServiceBlockingStub taskBlockingStub(ManagedChannel todoChannel) {
-        final TaskServiceGrpc.TaskServiceBlockingStub stub = TaskServiceGrpc.newBlockingStub(todoChannel);
+        return attachJwt(TaskServiceGrpc.newBlockingStub(todoChannel));
+    }
+
+    @Bean
+    @VaadinSessionScope
+    public ProjectServiceGrpc.ProjectServiceBlockingStub projectBlockingStub(ManagedChannel todoChannel) {
+        return attachJwt(ProjectServiceGrpc.newBlockingStub(todoChannel));
+    }
+
+    private <T extends AbstractStub<T>> T attachJwt(T stub) {
         final String jwt = (String) VaadinSession.getCurrent().getAttribute("jwt");
         if (jwt != null) {
             final Metadata.Key<String> jwtKey = Metadata.Key.of("jwt", Metadata.ASCII_STRING_MARSHALLER);
@@ -39,11 +48,5 @@ public class TodoConfiguration {
             return MetadataUtils.attachHeaders(stub, metadata);
         }
         return stub;
-    }
-
-    @Bean
-    @VaadinSessionScope
-    public ProjectServiceGrpc.ProjectServiceBlockingStub projectBlockingStub(ManagedChannel todoChannel) {
-        return ProjectServiceGrpc.newBlockingStub(todoChannel);
     }
 }
